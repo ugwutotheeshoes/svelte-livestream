c032ba296e8b2cf8677d11a328e5c6275b61475b
Emmanuel Ugwu <>
2022-07-18 17:31:20 +0100

    made changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,35 +1,32 @@
 <script>
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  import logo from "./assets/svelte.png";
+  import Counter from "./components/Counter.svelte";
+  import VideoPlayer from "./components/VideoPlayer.svelte";
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!</h1>
-
-  <Counter />
-
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
+  <h1>Live Simulation</h1>
+  <div class="video">
+    <VideoPlayer />
+  </div>
 </main>
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
 
   main {
     text-align: center;
     padding: 1em;
     margin: 0 auto;
+  }
+
+  .video {
+    width: 700px;
+    height: 550px;
+    display: inline-block;
   }
 
   img {
@@ -40,26 +37,21 @@
   h1 {
     color: #ff3e00;
     text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
+    font-size: 3rem;
+    font-weight: 300;
     line-height: 1.1;
     margin: 2rem auto;
-    max-width: 14rem;
+    /* max-width: 14rem; */
   }
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
+  @media (max-width: 750px) {
     h1 {
-      max-width: none;
+      font-size: 2rem;
     }
 
-    p {
-      max-width: none;
+    .video {
+      width: 400px;
+      height: 250px;
     }
   }
 </style>
---
+++ src/components/VideoPlayer.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Player, Video } from "@vime/svelte";
+</script>
+
+<Player controls >
+  <div class="video">
+    <Video >
+      <source
+        data-src="https://res.cloudinary.com/ugwutotheeshoes/video/upload/v1658160734/samples/mac%20miller.mp4"
+        type="video/mp4"
+      />
+    </Video>
+  </div>
+</Player>
