4b375b8d233b22de3bac0cb6ae3133b498733b8f
Emmanuel Ugwu <>
2022-07-19 23:54:49 +0100

    made changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,8 @@
 <script>
-  import logo from "./assets/svelte.png";
-  import Counter from "./components/Counter.svelte";
   import VideoPlayer from "./components/VideoPlayer.svelte";
 </script>
 
 <main>
-  <h1>Live Simulation</h1>
   <div class="video">
     <VideoPlayer />
   </div>
@@ -18,40 +15,19 @@
   }
 
   main {
-    text-align: center;
+    display: table;
     padding: 1em;
-    margin: 0 auto;
+    margin: 2rem auto;
   }
 
   .video {
     width: 700px;
-    height: 550px;
     display: inline-block;
   }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 3rem;
-    font-weight: 300;
-    line-height: 1.1;
-    margin: 2rem auto;
-    /* max-width: 14rem; */
-  }
-
   @media (max-width: 750px) {
-    h1 {
-      font-size: 2rem;
-    }
-
     .video {
       width: 400px;
-      height: 250px;
     }
   }
 </style>
--- src/components/VideoPlayer.svelte
+++ src/components/VideoPlayer.svelte
@@ -1,14 +1,128 @@
 <script>
+  // @ts-nocheck
+
   import { Player, Video } from "@vime/svelte";
+  import Icon from "svelte-icons-pack/Icon.svelte";
+  import RedoOutlined from "svelte-icons-pack/ai/AiOutlineRedo";
+  let toMute = true;
+  let streamEnded;
+  let playbackEnded = false;
+  let currentTime = 307;
+  let playing = true;
+  let playerControl = true;
+
+  const restart = () => {
+    currentTime = 0;
+    playerControl = true;
+    playbackEnded = false;
+  };
+
+  const videoEnded = () => {
+    playerControl = false;
+    playbackEnded = true;
+    streamEnded = "this stream has ended";
+  };
 </script>
 
-<Player controls >
-  <div class="video">
-    <Video >
-      <source
-        data-src="https://res.cloudinary.com/ugwutotheeshoes/video/upload/v1658160734/samples/mac%20miller.mp4"
-        type="video/mp4"
-      />
-    </Video>
-  </div>
-</Player>
+{#if playing}
+  <Player
+    controls={playerControl}
+    autoplay
+    muted={toMute}
+    on:vmPlaybackEnded={videoEnded}
+    {currentTime}
+  >
+    <div class="video">
+      <Video>
+        <source
+          data-src="https://res.cloudinary.com/ugwutotheeshoes/video/upload/v1658160734/samples/mac%20miller.mp4"
+          type="video/mp4"
+        />
+      </Video>
+    </div>
+  </Player>
+  <h3>Live: Mac miller Tiny Desk Concert</h3>
+  <p>1,150 views</p>
+  <p>streamed live 5 hours ago</p>
+  {#if playbackEnded}
+    <button on:click={restart}
+      >restart live video
+      <Icon
+        src={RedoOutlined}
+        color="white"
+        size="20"
+        className="custom-icon"
+        title="Custom icon params"
+      /></button
+    >
+    <p class="pos">{streamEnded}</p>
+  {/if}
+{/if}
+
+<style>
+  :global(vm-playback-control) {
+    --vm-control-scale: 1.7;
+  }
+
+  :global(.custom-icon) {
+    margin-left: 3px;
+    transform: rotate(20deg);
+  }
+  h3 {
+    color: #000;
+    text-transform: uppercase;
+    font-size: 1.2rem;
+    font-weight: 500;
+    line-height: 1.1;
+    margin: 0.4rem auto;
+    text-align: left;
+  }
+
+  p {
+    font-size: 0.9rem;
+    color: #000;
+    text-transform: uppercase;
+    font-weight: 500;
+    line-height: 1.1;
+    margin: 0.4rem auto;
+    text-align: left;
+  }
+
+  .pos {
+    position: absolute;
+    top: 32%;
+    left: 29%;
+    font-size: 2rem;
+    letter-spacing: 2px;
+    color: #fff;
+    font-weight: 400;
+  }
+  button {
+    position: absolute;
+    display: flex;
+    justify-content: center;
+    top: 43%;
+    left: 37%;
+    font-size: 1.2rem;
+    padding: 6px;
+    letter-spacing: 2px;
+    font-weight: 400;
+    border: none;
+    border-radius: 25px;
+    background: transparent;
+    cursor: pointer;
+    text-transform: uppercase;
+    color: white;
+  }
+
+  @media (max-width: 750px) {
+    h3 {
+      font-size: 1rem;
+      margin: 0.2rem auto;
+    }
+
+    p {
+      font-size: 0.7rem;
+    }
+  }
+</style>
